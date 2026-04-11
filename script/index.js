a7a3d4453f2bd03568374c8a5b1e5367959fb737
Ingrid <193829408+Ingrid-Almeida-developer@users.noreply.github.com>
2026-04-11 19:54:48 -0300

    trocando o path 2

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ const animaBut = lottie.loadAnimation({
 	renderer: 'svg',
 	loop: false,
 	autoplay: false,
-	path: 'https://assets8.lottiefiles.com/packages/lf20_nzuitqg1.json'
+	path: 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json'
 });
 
 let aberto = false;
