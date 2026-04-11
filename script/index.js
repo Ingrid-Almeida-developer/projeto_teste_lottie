7c2c4f2646e522384b30a4eceb5a663bb030789a
Ingrid <193829408+Ingrid-Almeida-developer@users.noreply.github.com>
2026-04-10 23:19:07 -0300

    Adicionando arquivos e testando api lottie pela primeira vez com menu hamburguer

---
+++ script/index.js
@@ -0,0 +1,11 @@
+const animaBut = lottie.loadAnimation({
+	container: document.getElementById("botao"),
+	renderer: 'svg',
+	loop: false,
+	autoplay: false,
+	path: 'https://assets9.lottiefiles.com/packages/lf20_touohxv0.json'
+});
+
+let aberto = false;
+
+
